cb475b81ec53d118978ef911ff28104c689999bb
joaoNogueiraDev <devjoaoluiz@gmail.com>
2024-05-09 12:06:07 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "primeflex/primeflex.css"
+import "primeicons/primeicons.css"
+import "primereact/resources/themes/lara-light-amber/theme.css";
+import { Button } from "primereact/button"
+
+
+const App = () => {
+  return(
+    <>
+      <Button label={"teste"}/>
+    </>
+  );
+}
+
+export default App;
